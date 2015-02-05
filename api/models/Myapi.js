2281787a19cabd56e62eea49aafb4f3d73ca1050
ishwarya <ish.ishw@gmail.com>
2015-02-05 08:47:25 +0000

    my first commit

---
+++ api/models/Myapi.js
@@ -0,0 +1,23 @@
+/**
+* Myapi.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = { 
+ 
+  attributes: { 
+  
+      name:{ 
+        type:"string", 
+        required:true, 
+        minLength: 2 
+      },     
+      email:{ 
+        type:"email", 
+        required:"true", 
+        unique: true 
+      }
+  } 
+}; 
